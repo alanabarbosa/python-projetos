ce1ecbab9064be1e5cf9ff0cd1dc561f8d634620
barbosaalana <alana@prumotecnologia.com.br>
2023-08-26 15:48:49 -0300

    Adds the option to restart game

--- guess_number.py
+++ guess_number.py
@@ -1,13 +1,31 @@
 import random
 
+win = 0
+
 while True:
-    rand_num = random.getrandbits(8)
-    print(rand_num)
+    rand_num = random.getrandbits(2)
     number = input("Digite um número: ")
+
     if number.isdigit():       
         if int(number) == rand_num:
-            print('Parabéns, você acertou!')
+            print('\nParabéns, você acertou! \n')
+            win += 1
+            print(f'\nPontuação: {win}\n')
+
+            question = input('Quer jogar novamente? (S/N) ')
+
+            if question.upper() == 'S':
+                continue    
+            else:
+                exit()                
+
         else:
-            print('Você errou!')
+            print('\nVocê errou! \n')
+            question = input('Quer jogar novamente? (S/N) ')
+
+            if question.upper() == 'S':
+                continue
+            else:
+                exit()   
     else:
-        print("Por favor, digite apenas números.")
+        print("\nPor favor, digite apenas números.\n")
